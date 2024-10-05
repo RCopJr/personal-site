f5b39809bb46087bb8af8aa4a490ecc178dabb01
Ramonito Copreros Jr. <rcoprerosjr@gmail.com>
2024-10-06 00:07:52 +0100

    Installed FontAwesome and added social media + footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+  faStrava,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef } from "react";
 
 const Header = () => {
@@ -20,17 +27,66 @@ const Header = () => {
   }, []);
 
   return (
-    <header ref={headerRef} className="py-24">
-      <h1 className="font-bold text-5xl text-neutral-200">Ramonito Copreros</h1>
-      <h2 className="mt-4 text-xl font-light text-neutral-200">
-        Frontend/Fullstack{" "}
-        <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 animate-gradient-slow bg-[length:200%_200%]">
-          Software Engineer
-        </span>
-      </h2>
-      <p className="mt-4 text-m max-w-xs text-neutral-400">
-        Aspiring to build interesting apps at the highest quality.
-      </p>
+    <header ref={headerRef} className="py-24 flex flex-col justify-between">
+      <div>
+        <h1 className="font-bold text-5xl text-neutral-200">
+          Ramonito Copreros
+        </h1>
+        <h2 className="mt-4 text-xl font-light text-neutral-200">
+          Frontend/Fullstack{" "}
+          <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 animate-gradient-slow bg-[length:200%_200%]">
+            Software Engineer
+          </span>
+        </h2>
+        <p className="mt-4 text-m max-w-xs text-neutral-400">
+          Aspiring to build interesting apps at the highest quality.
+        </p>
+      </div>
+      <div className="flex gap-7">
+        <a
+          href="https://github.com/RCopJr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="text-3xl text-neutral-100"
+            icon={faGithub}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/rcoprerosjr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="text-3xl text-neutral-100"
+            icon={faLinkedin}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a
+          href="https://www.instagram.com/ramonito.jr/?hl=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="text-3xl text-neutral-100"
+            icon={faInstagram}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a
+          href="https://www.strava.com/athletes/138075895"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            className="text-3xl text-neutral-100"
+            icon={faStrava}
+          ></FontAwesomeIcon>
+        </a>
+      </div>
     </header>
   );
 };
--- components/Resume.tsx
+++ components/Resume.tsx
@@ -147,6 +147,31 @@ const Resume = () => {
           </div>
         </a>
       </section>
+      <footer className="flex flex-col gap-1">
+        <div>
+          Deployed with <span>Vercel</span>. Built with{" "}
+          <span className="text-neutral-100">Next.js</span>,{" "}
+          <span className="text-neutral-100">Tailwind</span>, and{" "}
+          <span className="text-neutral-100">Love</span> ❤️
+        </div>
+        <div>
+          Main inspirations:{" "}
+          <a
+            href="https://brittanychiang.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-neutral-100">brittanychiang.com</span> and{" "}
+          </a>
+          <a
+            href="https://cherupil.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-neutral-100">cherupil.com</span>
+          </a>
+        </div>
+      </footer>
       {/* Simulating long content */}
       {/* {[...Array(50)].map((_, i) => (
         <div key={i} className="my-4">

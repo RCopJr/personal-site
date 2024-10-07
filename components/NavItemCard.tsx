5af236129640d4d93ec1a7f5b75be88a5bb8b3bd
Ramonito Copreros Jr. <rcoprerosjr@gmail.com>
2024-10-07 19:45:46 +0100

    added small navbar animation on select

--- components/NavItemCard.tsx
+++ components/NavItemCard.tsx
@@ -13,9 +13,12 @@ const NavItemCard = ({
     <a
       className={`${
         isActive ? "text-neutral-100 bg-pos-100 text-base" : ""
-      } py-2 w-32 text-sm rounded-xl duration-500 bg-gradient-to-r from-neutral-950 via-neutral-950 to-neutral-900 bg-size-200 transition-all`}
+      } flex py-2 pr-3 w-32 text-sm rounded-xl duration-500 bg-gradient-to-r from-neutral-950 via-neutral-950 to-neutral-900 bg-size-200 transition-all`}
       href={sectionLink}
     >
+      <div
+        className={`${isActive ? "w-full" : "w-0"} transform-all duration-500`}
+      ></div>
       {title.charAt(0).toUpperCase() + title.slice(1)}
     </a>
   );

cfba91f3f662327e8ce912f781cce8cffe8d4414
Ramonito Copreros Jr. <rcoprerosjr@gmail.com>
2024-10-08 15:18:28 +0100

    add shootings stars and stars background animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
 import Resume from "@/components/Resume";
+import { ShootingStars } from "@/components/UI/ShootingStars";
 import { Spotlight } from "@/components/UI/Spotlight";
+import { StarsBackground } from "@/components/UI/StarsBackground";
 
 export default function Home() {
   return (
     <main className="flex flex-col lg:grid lg:grid-cols-2 lg:gap-4 px-6 sm:px-12 lg:px-24 mx-auto max-w-screen-xl">
       {/* <Spotlight className="left-[0%] -top-[30%] w-[100%]" /> */}
-      <Spotlight className="left-[80%] -top-[10%] h-[40%]" />
-      <Spotlight className="left-[40%] top-[10%] h-[110%] " />
+      {/* <Spotlight className="left-[80%] -top-[10%] h-[40%]" /> */}
+      {/* <Spotlight className="left-[40%] top-[10%] h-[110%] " /> */}
+      <ShootingStars className="fixed -z-10" />
+      <StarsBackground className="fixed -z-10" />
       <Header />
       <Resume />
     </main>

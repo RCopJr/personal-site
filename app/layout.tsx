1bd6f252490c3580a8c30edd9891bde8493d9a4a
Ramonito Copreros Jr. <rcoprerosjr@gmail.com>
2024-10-09 18:06:23 +0100

    Update string formatting in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="en">
-      <body className={inter.className + " bg-gray-950 text-gray-400"}>
+      <body className={`${inter.className} bg-slate-900 text-slate-400`}>
         {children}
       </body>
     </html>

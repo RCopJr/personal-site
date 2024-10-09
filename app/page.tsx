f96e2e03b1f4a84fb3be48bcf3da4d8ca4c062e3
Ramonito Copreros Jr. <rcoprerosjr@gmail.com>
2024-10-09 18:29:56 +0100

    Add InitialMask to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import InitialMask from "@/components/InitialMask";
 import Header from "@/components/Sections/Header";
 import Resume from "@/components/Sections/Resume";
 import { ShootingStars } from "@/components/UI/ShootingStars";
@@ -5,11 +6,14 @@ import { StarsBackground } from "@/components/UI/StarsBackground";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex max-w-screen-xl flex-col px-6 sm:px-12 lg:grid lg:grid-cols-2 lg:gap-4 lg:px-24">
-      <ShootingStars className="fixed -z-10" />
-      <StarsBackground className="fixed -z-10" />
-      <Header />
-      <Resume />
-    </main>
+    <>
+      <InitialMask />
+      <main className="mx-auto flex max-w-screen-xl flex-col px-6 sm:px-12 lg:grid lg:grid-cols-2 lg:gap-4 lg:px-24">
+        <ShootingStars className="fixed -z-10 motion-reduce:hidden" />
+        <StarsBackground className="fixed -z-10 motion-reduce:hidden" />
+        <Header />
+        <Resume />
+      </main>
+    </>
   );
 }

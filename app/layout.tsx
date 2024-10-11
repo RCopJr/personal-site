7c737779b1f655eab677b751bbc89f2390d7c4aa
Ramonito Copreros Jr. <rcoprerosjr@gmail.com>
2024-10-11 13:30:22 +0100

    Update metadata and add thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ramonito Copreros Jr | Software Engineer",
   description: "Ramonito Copreros Jr's developer portfolio",
+  openGraph: {
+    title: "Ramonito Copreros Jr | Software Engineer",
+    siteName: "Ramonito Copreros Jr | Software Engineer",
+    description: "Ramonito Copreros Jr's Personal Website",
+    images: [
+      {
+        url: "https://www.ramonitocoprerosjr.vercel.app/thumbnail.png",
+        alt: "Ramonito Copreros Jr's Personal Website",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
